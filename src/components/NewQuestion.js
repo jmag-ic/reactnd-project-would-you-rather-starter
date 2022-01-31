5944c7e2a52d6a95447d69a26a64f298f0e98706
Jose Guzman <josemczg@gmail.com>
2022-01-31 10:44:31 -0600

    Add new question feature

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleCreateQuestion } from '../actions/questions';
 
 class NewQuestion extends Component {
+  
+  state = {
+    optionOne: '',
+    optionTwo: ''
+  };
+
+  onFormChange = (option, value) => {
+    this.setState({[option]: value});
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const { dispatch, authedUser } = this.props;
+    const { optionOne, optionTwo } = this.state;
+    dispatch(handleCreateQuestion(optionOne, optionTwo, authedUser));
+  };
+
   render() {
     return (
-      <div>New Question</div>
+      <div>
+        <h1>New Question</h1>
+        <p>Complete the question:</p>
+        <h3>Would you rather...</h3>
+        <form onSubmit={this.onSubmit}>
+          <input
+            placeholder="Enter Option One Text Here"
+            value={this.state.optionOne}
+            onChange={(event) => this.onFormChange('optionOne', event.target.value)}
+            />
+          <input
+            placeholder="Enter Option Two Text Here"
+            value={this.state.optionTwo}
+            onChange={(event) => this.onFormChange('optionTwo', event.target.value)}
+            />
+          <button>Submit</button>
+        </form>
+      </div>
     );
   }
 }
 
-export default NewQuestion;
+export default connect(({ authedUser }) => ({
+  authedUser
+}))(NewQuestion);
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,18 @@
+import { CREATE_QUESTION } from '../actions/questions';
 import { ANSWER_QUESTION } from '../actions/shared';
 import { RECEIVE_USERS } from '../actions/users';
 
 export default function users (state = {}, action) {
   switch(action.type) {
+    case CREATE_QUESTION:
+      const { author, id } = action.question;
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          questions: [...state[author].questions, id]
+        }
+      };
     case ANSWER_QUESTION:
       const { qid, answer, authedUser } = action;
       return {
