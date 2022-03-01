19bfcbad9d29b34361b3846482c33f4dd054b4e8
José Guzmán <josemczg@gmail.com>
2022-03-01 14:55:12 -0600

    Add question item styles

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Card from './Card';
+import '../styles/QuestionItem.css';
 
 function QuestionItem(props) {
   return (
@@ -11,7 +12,9 @@ function QuestionItem(props) {
       imageURL={props.author.avatarURL}>
       <h2>Would you rather</h2>
       <p>...{props.question.optionOne.text.substr(0, 15)}...</p>
-      <Link to={`questions/${props.question.id}`}>View Poll</Link>
+      <div className="view-poll">
+        <Link to={`questions/${props.question.id}`}>View Poll</Link>
+      </div>
     </Card>
   );
 }
