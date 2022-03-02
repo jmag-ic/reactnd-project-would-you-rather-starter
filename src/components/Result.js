34938bc44dcc6d6f5fd92cd05d2df5697e89ebb1
José Guzmán <josemczg@gmail.com>
2022-03-02 00:49:30 -0600

    Add Result styles

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "../styles/Result.css";
 
 function Result(props) {
+  const percent = (props.votes / props.total * 100);
   return (
-    <div className="result">
-      {props.voted && <div>Your vote</div>}
+    <div className={props.voted ? 'voted result' : 'result'}>
+      {props.voted && <div className="your-vote">
+        <span>Your</span>
+        <span>vote</span>
+      </div>}
       <p className="option">{`Would you rather ${props.option}?`}</p>
-      <div className="precentage">{`${props.votes / props.total * 100}%`}</div>
+      <div className="percentage">
+        <div className="total"></div>
+        <div
+          className={percent > 0 ? 'value' : 'value empty'}
+          style={{ width: `${percent}%` }}>  
+        </div>
+        <span className="label">{`${percent}%`}</span>
+      </div>
       <p className="counter">{`${props.votes} out of ${props.total} votes`}</p>
     </div>
   );
