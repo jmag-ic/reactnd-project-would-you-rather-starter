23cc17e9517cd5878fe819ea795a3e81829715ff
José Guzmán <josemczg@gmail.com>
2022-03-05 00:35:45 -0600

    Add some text to page not found component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function PageNotFound() {
   return (
-  <div>404</div>
+    <div>
+      <div>Oops! Page not found</div>
+      <div>Error 404</div>
+    </div>
   );
 }
